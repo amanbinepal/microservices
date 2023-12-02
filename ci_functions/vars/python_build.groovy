aec2262796e7e90a0a1159c3374e9ee0bfcc4b90
Amanpreet <abinepal@my.bcit.ca>
2023-12-01 16:10:51 -0800

    added dockerRepoName

--- ci_functions/vars/python_build.groovy
+++ ci_functions/vars/python_build.groovy
@@ -20,53 +20,62 @@ def call(dockerRepoName, imageName, portNum) {
 			}
 			stage('Python Lint') {
 				steps {
-                    sh 'pylint --fail-under 5.0 *.py'
+					dir("${dockerRepoName}") {
+                		sh 'pylint --fail-under 5.0 *.py'
+					}
 				}
 			}
 			stage('Test and Coverage') {
 				steps {
-					script {
-						def test_reports_exist = fileExists 'test-reports'
-						if (test_reports_exist) {
-							sh 'rm test-reports/*.xml || true'
-						}
-						def api_test_reports_exist = fileExists 'api-test-reports'
-						if (api_test_reports_exist) {
-							sh 'rm api-test-reports/*.xml || true'
-						}
-					}
-					script {
-						def testFiles = findFiles(glob: '**/test*.py')
-						
-						if (testFiles) {
-							for (def file in testFiles) {
-								// Run each test file using the 'sh' step (assuming you're running Python tests)
-								sh "coverage run --omit */site-packages/*,*/dist-packages/* ${file}"
+					dir("${dockerRepoName}") {
+						script {
+							def test_reports_exist = fileExists 'test-reports'
+							if (test_reports_exist) {
+								sh 'rm test-reports/*.xml || true'
 							}
-							sh "coverage report"
-						} else {
-							echo "No test files found in the workspace."
+							def api_test_reports_exist = fileExists 'api-test-reports'
+							if (api_test_reports_exist) {
+								sh 'rm api-test-reports/*.xml || true'
+							}
+						}
+						script {
+							def testFiles = findFiles(glob: '**/test*.py')
+							
+							if (testFiles) {
+								for (def file in testFiles) {
+									// Run each test file using the 'sh' step (assuming you're running Python tests)
+									sh "coverage run --omit */site-packages/*,*/dist-packages/* ${file}"
+								}
+								sh "coverage report"
+							} else {
+								echo "No test files found in the workspace."
+							}
 						}
 					}
 				}
 				post {
 					always {
-						script {
-							def test_reports_exist = fileExists 'test-reports'
-							if (test_reports_exist) {
-								junit 'test-reports/*.xml'
-							}
-							def api_test_reports_exist = fileExists 'api-test-reports'
-							if (api_test_reports_exist) {
-								junit 'api-test-reports/*.xml'
+						dir("${dockerRepoName}") {
+							script {
+								def test_reports_exist = fileExists 'test-reports'
+								if (test_reports_exist) {
+									junit 'test-reports/*.xml'
+								}
+								def api_test_reports_exist = fileExists 'api-test-reports'
+								if (api_test_reports_exist) {
+									junit 'api-test-reports/*.xml'
+								}
 							}
 						}
 					}
+
 				}
 			}
 			stage ('Zip Archive') {
 				steps {
-					sh 'zip app.zip *.py'
+					dir("${dockerRepoName}") {
+						sh 'zip app.zip *.py'
+					}
 				}
 				post {
 					always {
@@ -81,8 +90,10 @@ def call(dockerRepoName, imageName, portNum) {
 				steps {
 					withCredentials([usernamePassword(credentialsId: 'b394d855-0512-4dc6-b554-e738752982c5', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
 						sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin docker.io'
-						sh "docker build -t ${dockerRepoName}:latest --tag amanbinepal/${dockerRepoName}:${imageName} ."
-						sh "docker push amanbinepal/${dockerRepoName}:${imageName}"
+						dir("${dockerRepoName}") {
+							sh "docker build -t ${dockerRepoName}:latest --tag amanbinepal/${dockerRepoName}:${imageName} ."
+							sh "docker push amanbinepal/${dockerRepoName}:${imageName}"
+						}
 					}
 				}
 			}
